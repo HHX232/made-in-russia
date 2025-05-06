b240990e09ea40a5b052abbd0ba4d8586403be7d
HHX232 <140748811+HHX232@users.noreply.github.com>
2025-05-06 23:45:47 +0300

    site map

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import Ads from '@/components/screens/Ads/Ads'
 import Header from '@/components/MainComponents/Header/Header'
 import {FC} from 'react'
--- src/services/auth/auth.helper.ts
+++ src/services/auth/auth.helper.ts
@@ -2,28 +2,28 @@
 
 import Cookies from 'js-cookie'
 
-export const saveTokenStorage = (data: any) =>{
-   Cookies.set('accessToken', data.accessToken)
-   Cookies.set('refreshToken', data.refreshToken)
+export const saveTokenStorage = (data: any) => {
+  Cookies.set('accessToken', data.accessToken)
+  Cookies.set('refreshToken', data.refreshToken)
 }
 
-export const removeFromStorage = ():void =>{
-   Cookies.remove('accessToken')
-   Cookies.remove('refreshToken')
-   localStorage.removeItem('user')
+export const removeFromStorage = (): void => {
+  Cookies.remove('accessToken')
+  Cookies.remove('refreshToken')
+  localStorage.removeItem('user')
 }
 
-export const saveToStorage = (data: any):void => {
-   saveTokenStorage(data)
-   localStorage.setItem('user', JSON.stringify(data.user))
+export const saveToStorage = (data: any): void => {
+  saveTokenStorage(data)
+  localStorage.setItem('user', JSON.stringify(data.user))
 }
 
-export const getAccessToken = async () =>{
-   const accessToken = Cookies.get('accessToken')
-   return accessToken || null
+export const getAccessToken = async () => {
+  const accessToken = Cookies.get('accessToken')
+  return accessToken || null
 }
 
-export const getRefreshToken = async () =>{
-   const refreshToken = Cookies.get('refreshToken')
-   return refreshToken || null
+export const getRefreshToken = async () => {
+  const refreshToken = Cookies.get('refreshToken')
+  return refreshToken || null
 }
