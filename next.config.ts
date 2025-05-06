c2036784a53bfce3f4fdc612530653462053ba19
HHX232 <140748811+HHX232@users.noreply.github.com>
2025-05-07 00:30:02 +0300

    config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type {NextConfig} from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -6,5 +7,10 @@ const nextConfig: NextConfig = {
     domains: ['loremflickr.com', 'www.aptronixindia.com', 'lesoteka.com', 'images.unsplash.com', 'www.thekint.com']
   }
 }
-
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@/components'] = path.resolve(__dirname, 'src/components')
+    return config
+  }
+}
 export default nextConfig
