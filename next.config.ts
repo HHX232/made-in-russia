9f4e8c9e64a5453aacbcf852db0e933695315962
HHX232 <140748811+HHX232@users.noreply.github.com>
2025-09-02 16:01:11 +0300

    add link

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,8 @@ const nextConfig: NextConfig = {
       'plus.unsplash.com',
       'res.cloudinary.com',
       'TEMP_URL',
-      'https://lh3.googleusercontent.com'
+      'https://lh3.googleusercontent.com',
+      'https://6e9e2c8a-521e-4620-9cf1-ef51353051d3.srvstatic.uz'
     ]
   }
 }
