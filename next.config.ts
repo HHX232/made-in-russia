e785af28a3eaf2117368a7b4b33199ac4fc28833
HHX232 <140748811+HHX232@users.noreply.github.com>
2025-09-23 23:41:36 +0300

    update -oldConfig-

--- next.config.ts
+++ next.config.ts
@@ -28,7 +28,12 @@ const nextConfig: NextConfig = {
       'https://6e9e2c8a-521e-4620-9cf1-ef51353051d3.srvstatic.uz',
       '6e9e2c8a-521e-4620-9cf1-ef51353051d3.srvstatic.uz',
       't.me',
-      'exporteru.b-cdn.net'
+      'exporteru.b-cdn.net',
+      'en.exporteru.com',
+      'exporteru.com',
+      'cn.exporteru.com',
+      'exporteru.b-cdn.net',
+      'https://exporteru.b-cdn.net'
     ]
   }
 }
