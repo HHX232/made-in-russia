5018f7d26d66942956e76ba88dfd981b3bf48005
HHX232 <140748811+HHX232@users.noreply.github.com>
2025-10-21 00:25:13 +0300

    main categiry page fixed name

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,6 +1,6 @@
 import Footer from '@/components/MainComponents/Footer/Footer'
 import Header from '@/components/MainComponents/Header/Header'
-import MainCategoryPage from '@/components/pages/MainCategoryPage /MainCategoryPage'
+import MainCategoryPage from '@/components/pages/MainCategoryPage/MainCategoryPage'
 import Catalog from '@/components/screens/Catalog/Catalog'
 import {getCurrentLocale} from '@/lib/locale-detection'
 import CategoriesService from '@/services/categoryes/categoryes.service'
