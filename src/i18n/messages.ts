fac143d3eb68e667d8de702d696381d0687a18e1
Roman <franck93@mail.ru>
2025-12-15 10:18:50 +0400

    Implemented chat

---
+++ src/i18n/messages.ts
@@ -0,0 +1,11 @@
+import en from '../../messages/en.json'
+import ru from '../../messages/ru.json'
+import zh from '../../messages/zh.json'
+import hi from '../../messages/hi.json'
+
+export const messages = {
+  en,
+  ru,
+  zh,
+  hi
+} as const
